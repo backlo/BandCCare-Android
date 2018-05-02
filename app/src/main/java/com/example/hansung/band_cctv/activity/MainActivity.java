627f2b3d01c64624729eae673dad1b2e66612d05
lenovo <hjun4512@naver.com>
2018-05-02 18:15:19 +0900

    Merge branch 'master' of C:\Program Files\Android\AndroidProject\Band_CCTV with conflicts.

--- app/src/main/java/com/example/hansung/band_cctv/activity/MainActivity.java
+++ app/src/main/java/com/example/hansung/band_cctv/activity/MainActivity.java
@@ -1,41 +1,25 @@
 package com.example.hansung.band_cctv.activity;
 
-import android.os.Message;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.Handler;
+import android.view.SurfaceView;
+import android.widget.RelativeLayout;
 
-import com.example.hansung.band_cctv.MyPagerAdapter;
 import com.example.hansung.band_cctv.R;
-
-import android.support.design.widget.TabLayout;
+import com.example.hansung.band_cctv.util.RtspViewPlayer;
 
 public class MainActivity extends AppCompatActivity {
-    public static MyHandler myHandler;
+    private RelativeLayout surfaceView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        RtspViewPlayer playView = new RtspViewPlayer(getApplication(),"rtsp://192.168.0.2:8091/rtsp");
 
-        myHandler = new MyHandler();
-        MyPagerAdapter mPagerAdapter = new MyPagerAdapter(getSupportFragmentManager());
-        ViewPager mViewPager = (ViewPager) findViewById(R.id.viewpager);
-        mViewPager.setAdapter(mPagerAdapter);
+        surfaceView = (RelativeLayout) findViewById(R.id.surface_video);
+        surfaceView.addView(playView);
 
-        TabLayout mTab = (TabLayout) findViewById(R.id.tabs);
-        mTab.setupWithViewPager(mViewPager);
-
-        for (int i = 0; i < mViewPager.getAdapter().getCount(); i++) {
-            mTab.getTabAt(i).setIcon(mPagerAdapter.getIcon(i));
-        }
-    }
-
-    public class MyHandler extends Handler {
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-        }
     }
 }
