f1d52b90e11fdd6ef8735da2bc137fa0cf0046ae
hjun4512@naver.com <hjun4512@naver.com>
2018-05-31 16:39:22 +0900

    test

--- app/src/main/java/com/example/hansung/band_cctv/util/RtspViewPlayer.java
+++ app/src/main/java/com/example/hansung/band_cctv/util/RtspViewPlayer.java
@@ -1,4 +1,3 @@
-/*
 package com.example.hansung.band_cctv.util;
 
 import android.content.Context;
@@ -13,6 +12,7 @@ public class RtspViewPlayer extends SurfaceView implements SurfaceHolder.Callbac
     private SurfaceHolder mHolder;
     private NDKAdapter mPlayerNDKAdapter;
     private String uri;
+    private static int state;
 
     public RtspViewPlayer(Context context, String uri) {
         super(context);
@@ -35,6 +35,7 @@ public class RtspViewPlayer extends SurfaceView implements SurfaceHolder.Callbac
                     Log.e("surface run1 : ", mHolder.getSurface()+"");
                     mPlayerNDKAdapter.play(mHolder.getSurface());
                     Log.e("surface run2 : ", mHolder.getSurface()+"");
+                    state = 0;
             }
         }).start();
     }
@@ -47,72 +48,11 @@ public class RtspViewPlayer extends SurfaceView implements SurfaceHolder.Callbac
     @Override
     public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
         Log.e("surface destory1 : ", mHolder.getSurface()+"");
-        mPlayerNDKAdapter.stop(1);
-        Log.e("surface destory2 : ", mHolder.getSurface()+"");
-    }
-}
-*/
-package com.example.hansung.band_cctv.util;
-
-import android.content.Context;
-import android.util.Log;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-
-public class RtspViewPlayer extends SurfaceView implements SurfaceHolder.Callback {
-
-    private static final String TAG = "RtspPlayView";
-
-    private SurfaceHolder mHolder;
-    private NDKAdapter mPlayerNDKAdapter;
-    private String uri;
-    private boolean state = true;
-
-    public RtspViewPlayer(Context context, String uri) {
-        super(context);
-
-        this.uri = uri;
-        mHolder = getHolder();
-        mHolder.addCallback(this);
-
-        mPlayerNDKAdapter = new NDKAdapter();
-        Log.d("datasource",uri+"");
-
-    }
-
-    @Override
-    public void surfaceCreated(SurfaceHolder surfaceHolder) {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while(state == true){
-                    NDKAdapter.setDataSource(uri);
-                    Log.e("surface run1 : ", mHolder.getSurface()+"");
-                    mPlayerNDKAdapter.play(mHolder.getSurface());
-                    Log.e("surface run2 : ", mHolder.getSurface()+"");
-                    try {
-                        Thread.sleep(5000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                if(state == false){
-                    state = true;
-                }
-            }
-        }).start();
-    }
-
-    @Override
-    public void surfaceChanged(SurfaceHolder surfaceHolder, int format, int width, int height) {
-
-    }
-
-    @Override
-    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
-        Log.e("surface destory1 : ", mHolder.getSurface()+"");
-        mPlayerNDKAdapter.stop(1);
-        state = false;
+        if(state == 0){
+            mPlayerNDKAdapter.stop(0);
+        }else{
+            mPlayerNDKAdapter.stop(1);
+        }
         Log.e("surface destory2 : ", mHolder.getSurface()+"");
     }
 }
--- app/src/main/jni/interface.c
+++ app/src/main/jni/interface.c
@@ -16,21 +16,21 @@
 #define LOGW(...) __android_log_print(ANDROID_LOG_WARN   , "libnav", __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR  , "libnav", __VA_ARGS__)
 
-//const char *uri[2];
-//int setDataSource_index=0;
-//int play_index=0;
+const char *uri[2];
+int setDataSource_index=0;
+int play_index=0;
 
-char *uri;
-char *uri1;
+//char *uri;
+//char *uri1;
 int x=0;
-int y=0;
+//int y=0;
 
 JNIEXPORT void JNICALL Java_com_example_hansung_band_1cctv_util_NDKAdapter_setDataSource(JNIEnv *env, jclass clazz, jstring _uri){
-    //uri[setDataSource_index] = (*env)->GetStringUTFChars(env, _uri, NULL);
-    uri = (*env)->GetStringUTFChars(env, _uri, NULL);
-    //LOGD("uri:%s", uri[setDataSource_index]);
-    LOGD("uri:%s", uri);
-    //setDataSource_index++;
+    uri[setDataSource_index] = (*env)->GetStringUTFChars(env, _uri, NULL);
+    //uri = (*env)->GetStringUTFChars(env, _uri, NULL);
+    LOGD("uri:%s", uri[setDataSource_index]);
+    //LOGD("uri:%s", uri);
+    setDataSource_index++;
 
 }
 
@@ -39,11 +39,11 @@ JNIEXPORT jint JNICALL Java_com_example_hansung_band_1cctv_util_NDKAdapter_play(
     LOGD("play");
 
     // it must set to setDataSource for play.
-    //const char * file_name = uri[play_index];
-    const char * file_name = uri;
+    const char * file_name = uri[play_index];
+    //const char * file_name = uri;
 
-    //LOGD("uri:%s", uri[play_index]);
-    LOGD("uri:%s", uri);
+    LOGD("uri:%s", uri[play_index]);
+   // LOGD("uri:%s", uri);
     LOGD("file_name:%s", file_name);
     //play_index++;
     if( file_name == NULL ) {
@@ -198,183 +198,4 @@ JNIEXPORT jint JNICALL Java_com_example_hansung_band_1cctv_util_NDKAdapter_play(
 
 JNIEXPORT jint JNICALL Java_com_example_hansung_band_1cctv_util_NDKAdapter_stop(JNIEnv * env, jclass clazz, jint k){
     x=k;
-}
-
-
-
-
-
-JNIEXPORT void JNICALL Java_com_example_hansung_band_1cctv_util_NDKAdapter_setDataSource1(JNIEnv *env, jclass clazz, jstring _uri){
-    //uri[setDataSource_index] = (*env)->GetStringUTFChars(env, _uri, NULL);
-    uri1 = (*env)->GetStringUTFChars(env, _uri, NULL);
-    //LOGD("uri:%s", uri[setDataSource_index]);
-    LOGD("uri:%s", uri);
-    //setDataSource_index++;
-
-}
-
-JNIEXPORT jint JNICALL Java_com_example_hansung_band_1cctv_util_NDKAdapter_play1(JNIEnv * env, jclass clazz, jobject surface)
-{
-    LOGD("play");
-
-    // it must set to setDataSource for play.
-    //const char * file_name = uri[play_index];
-    const char * file_name = uri1;
-
-    //LOGD("uri:%s", uri[play_index]);
-    LOGD("uri:%s", uri1);
-    LOGD("file_name:%s", file_name);
-    //play_index++;
-    if( file_name == NULL ) {
-        LOGE("Please set the DataSource");
-        return -1;
-    }
-
-    av_register_all(); // get muxer, demuxer and protocol definitions.
-
-    AVFormatContext * pFormatCtx = avformat_alloc_context();
-
-    // Open video file
-    if(avformat_open_input(&pFormatCtx, file_name, NULL, NULL)!=0) {
-
-        LOGE("Couldn't open file:%s\n", file_name);
-        return -1; // Couldn't open file
-    }
-
-    // Retrieve stream information
-    if(avformat_find_stream_info(pFormatCtx, NULL)<0) {
-        LOGE("Couldn't find stream information.");
-        return -1;
-    }
-
-    // Find the first video stream
-    int videoStream = -1, i;
-    for (i = 0; i < pFormatCtx->nb_streams; i++) {
-        if (pFormatCtx->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO
-           && videoStream < 0) {
-            videoStream = i;
-        }
-    }
-    if(videoStream==-1) {
-        LOGE("Didn't find a video stream.");
-        return -1; // Didn't find a video stream
-    }
-
-    // Get a pointer to the codec context for the video stream
-    AVCodecContext  * pCodecCtx = pFormatCtx->streams[videoStream]->codec;
-
-    // Find the decoder for the video stream
-    AVCodec * pCodec = avcodec_find_decoder(pCodecCtx->codec_id);
-    if(pCodec==NULL) {
-        LOGE("Codec not found.");
-        return -1; // Codec not found
-    }
-
-    if(avcodec_open2(pCodecCtx, pCodec, NULL) < 0) {
-        LOGE("Could not open codec.");
-        return -1; // Could not open codec
-    }
-
-    // get native window that called by surface in java.
-    ANativeWindow* nativeWindow = ANativeWindow_fromSurface(env, surface);
-
-    // get codec size
-    int videoWidth = pCodecCtx->width;
-    int videoHeight = pCodecCtx->height;
-
-    // buffer size is decided native window size
-    ANativeWindow_setBuffersGeometry(nativeWindow,  videoWidth, videoHeight, WINDOW_FORMAT_RGBA_8888);
-    ANativeWindow_Buffer windowBuffer;
-
-    if(avcodec_open2(pCodecCtx, pCodec, NULL)<0) {
-        LOGE("Could not open codec.");
-        return -1; // Could not open codec
-    }
-
-    // Allocate video frame
-    AVFrame * pFrame = av_frame_alloc();
-
-    // rendering
-    AVFrame * pFrameRGBA = av_frame_alloc();
-    if(pFrameRGBA == NULL || pFrame == NULL) {
-        LOGE("Could not allocate video frame.");
-        return -1;
-    }
-
-    // Determine required buffer size and allocate buffer
-    int numBytes=av_image_get_buffer_size(AV_PIX_FMT_RGBA, pCodecCtx->width, pCodecCtx->height, 1);
-    uint8_t * buffer=(uint8_t *)av_malloc(numBytes*sizeof(uint8_t));
-    av_image_fill_arrays(pFrameRGBA->data, pFrameRGBA->linesize, buffer, AV_PIX_FMT_RGBA,
-                         pCodecCtx->width, pCodecCtx->height, 1);
-
-    // set stream-data-format from YUV to RGBA.
-    struct SwsContext *sws_ctx = sws_getContext(pCodecCtx->width,
-                             pCodecCtx->height,
-                             pCodecCtx->pix_fmt,
-                             pCodecCtx->width,
-                             pCodecCtx->height,
-                             AV_PIX_FMT_RGBA,
-                             SWS_BILINEAR,
-                             NULL,
-                             NULL,
-                             NULL);
-
-    int frameFinished;
-    AVPacket packet;
-    while(av_read_frame(pFormatCtx, &packet)>=0) {
-        // Is this a packet from the video stream?
-        if(packet.stream_index==videoStream) {
-
-            // Decode video frame
-            avcodec_decode_video2(pCodecCtx, pFrame, &frameFinished, &packet);
-
-            if (frameFinished) {
-
-                // lock native window buffer
-                ANativeWindow_lock(nativeWindow, &windowBuffer, 0);
-
-                // convert format
-                sws_scale(sws_ctx, (uint8_t const * const *)pFrame->data,
-                          pFrame->linesize, 0, pCodecCtx->height,
-                          pFrameRGBA->data, pFrameRGBA->linesize);
-
-                // calculate stride.
-                uint8_t * dst = windowBuffer.bits;
-                int dstStride = windowBuffer.stride * 4;
-                uint8_t * src = (uint8_t*) (pFrameRGBA->data[0]);
-                int srcStride = pFrameRGBA->linesize[0];
-
-                // Depending on the stride and gait window frame, thus requiring a progressive copy
-                int h;
-                for (h = 0; h < videoHeight; h++) {
-                    memcpy(dst + h * dstStride, src + h * srcStride, srcStride);
-                }
-
-                ANativeWindow_unlockAndPost(nativeWindow);
-            }
-
-        }
-
-        av_packet_unref(&packet);
-        if(y==1) break;
-    }
-
-    av_free(buffer);
-    av_free(pFrameRGBA);
-
-    // Free the YUV frame
-    av_free(pFrame);
-
-    // Close the codecs
-    avcodec_close(pCodecCtx);
-
-    // Close the video file
-    avformat_close_input(&pFormatCtx);
-    y=0;
-    return 0;
-
-}
-
-JNIEXPORT jint JNICALL Java_com_example_hansung_band_1cctv_util_NDKAdapter_stop1(JNIEnv * env, jclass clazz, jint k){
-    y=k;
 }
