b98c2535f5ebb90fb553f91ce2dbc90a5a0c235f
hjun4512@naver.com <hjun4512@naver.com>
2018-05-31 16:04:12 +0900

    ffmpeg threading to surfaceview11

--- app/src/main/java/com/example/hansung/band_cctv/util/RtspViewPlayer.java
+++ app/src/main/java/com/example/hansung/band_cctv/util/RtspViewPlayer.java
@@ -1,3 +1,4 @@
+/*
 package com.example.hansung.band_cctv.util;
 
 import android.content.Context;
@@ -47,6 +48,71 @@ public class RtspViewPlayer extends SurfaceView implements SurfaceHolder.Callbac
     public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
         Log.e("surface destory1 : ", mHolder.getSurface()+"");
         mPlayerNDKAdapter.stop(1);
+        Log.e("surface destory2 : ", mHolder.getSurface()+"");
+    }
+}
+*/
+package com.example.hansung.band_cctv.util;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class RtspViewPlayer extends SurfaceView implements SurfaceHolder.Callback {
+
+    private static final String TAG = "RtspPlayView";
+
+    private SurfaceHolder mHolder;
+    private NDKAdapter mPlayerNDKAdapter;
+    private String uri;
+    private boolean state = true;
+
+    public RtspViewPlayer(Context context, String uri) {
+        super(context);
+
+        this.uri = uri;
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+
+        mPlayerNDKAdapter = new NDKAdapter();
+        Log.d("datasource",uri+"");
+
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(state == true){
+                    NDKAdapter.setDataSource(uri);
+                    Log.e("surface run1 : ", mHolder.getSurface()+"");
+                    mPlayerNDKAdapter.play(mHolder.getSurface());
+                    Log.e("surface run2 : ", mHolder.getSurface()+"");
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if(state == false){
+                    state = true;
+                }
+            }
+        }).start();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+        Log.e("surface destory1 : ", mHolder.getSurface()+"");
+        mPlayerNDKAdapter.stop(1);
+        state = false;
         Log.e("surface destory2 : ", mHolder.getSurface()+"");
     }
 }
--- app/src/main/jni/com_example_hansung_band_cctv_util_NDKAdapter.h
+++ app/src/main/jni/com_example_hansung_band_cctv_util_NDKAdapter.h
@@ -31,6 +31,30 @@ JNIEXPORT jint JNICALL Java_com_example_hansung_band_1cctv_util_NDKAdapter_play
 JNIEXPORT jint JNICALL Java_com_example_hansung_band_1cctv_util_NDKAdapter_stop
   (JNIEnv *, jclass, jint);
 
+/*
+ * Class:     com_example_hansung_band_cctv_util_NDKAdapter
+ * Method:    setDataSource1
+ * Signature: (Ljava/lang/String;)V
+ */
+JNIEXPORT void JNICALL Java_com_example_hansung_band_1cctv_util_NDKAdapter_setDataSource1
+  (JNIEnv *, jclass, jstring);
+
+/*
+ * Class:     com_example_hansung_band_cctv_util_NDKAdapter
+ * Method:    play1
+ * Signature: (Ljava/lang/Object;)I
+ */
+JNIEXPORT jint JNICALL Java_com_example_hansung_band_1cctv_util_NDKAdapter_play1
+  (JNIEnv *, jclass, jobject);
+
+/*
+ * Class:     com_example_hansung_band_cctv_util_NDKAdapter
+ * Method:    stop1
+ * Signature: (I)I
+ */
+JNIEXPORT jint JNICALL Java_com_example_hansung_band_1cctv_util_NDKAdapter_stop1
+  (JNIEnv *, jclass, jint);
+
 #ifdef __cplusplus
 }
 #endif
